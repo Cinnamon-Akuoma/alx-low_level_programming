1ab25f2c5ac9eaca8d3480175811436d1c943378
Cinnamon-Akuoma <cboyle.george@gmail.com>
2022-09-18 14:13:26 -0700

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 /**
 *print_alphabet - prints lower case alphabet
 *
+*Return: Always 0 (Success)
 **/
 void print_alphabet(void)
 {
@@ -24,4 +25,5 @@ for (i = '0'; i <= '9'; i++)
 {
 print_alphabet();
 }
+return (0);
 }
