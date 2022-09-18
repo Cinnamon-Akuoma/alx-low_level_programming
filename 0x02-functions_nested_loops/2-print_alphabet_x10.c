610eff5016a4a3c6f6e0cab5bdeb7111a71e073e
Cinnamon-Akuoma <cboyle.george@gmail.com>
2022-09-18 14:59:03 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,29 +1,19 @@
 #include "main.h"
 /**
-*print_alphabet - prints lower case alphabet
+*print_alphabet_x10 - function to print abc 10 times
 *
-*Return: Always 0(Success)
-**/
-void print_alphabet(void)
+*Return: 0
+*/
+void print_alphabet_x10(void)
 {
 char c, i;
-for (c = 'a'; c <= 'z'; i++)
+
+for (i = 0; i <= 9; i++)
 {
-_putchar(i);
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
 }
 _putchar('\n');
 }
-
-/**
-*print_alphabet_x10 - prints alphabets 10 times
-*
-**/
-void print_alphabet_x10(void)
-{
-int i;
-for (i = '0'; i <= '9'; i++)
-{
-print_alphabet();
-}
-return (0);
 }
