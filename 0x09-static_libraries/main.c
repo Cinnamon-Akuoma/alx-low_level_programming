6ff541e086355c192f1877c49ef22b39bad8fce3
Cinnamon-Akuoma <cboyle.george@gmail.com>
2022-12-13 14:20:48 -0800

    Task Done

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* main- contains the quote
+* Return- return is zero
+*/ 
+int main(void)
+{
+_puts("\"In the end, my goals' to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
